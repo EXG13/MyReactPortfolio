69b3c6c5eeae32db26f85536d2ae2791c6f4b874
Tomer J. Lind <digitalassetshub@gmail.com>
2024-01-19 20:46:42 +0200

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,12 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Home />
-      <About />
-      <Projects />
-      <Contact/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </Router>
   );
 }

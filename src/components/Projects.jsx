9973a6d3e51a45fd5d4c1f30cc74fe82e2c0e85b
Tomer J. Lind <digitalassetshub@gmail.com>
2024-01-19 23:55:27 +0200

    last changes to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,87 +2,39 @@ import React from "react";
 import CelestialChronicles from "../assets/CelestialChronicles.png";
 import ResponsiveDesign from "../assets/ResponsiveDesign.png";
 import PortfolioWebsite from "../assets/PortfolioWebsite.png";
+import Pikachu from "../projects.json";
 
 function Projects() {
   return (
-    <section id="portfolio" class="p-5">
-      <div class="container">
-        <h2 class="text-center p-4"> Recent Projects</h2>
-        <div class="row text-center g-4">
-          <div class="col-md">
-            <div class="card bg-dark text-light projectCard1">
-              <img
-                src={CelestialChronicles}
-                class="w-100 card1-img"
-                alt="This is an image of my first project, which was a refactoring work for a marketing company called Horiseon - The image is showing their landing page"
-              />
-              <div class="card-body text-center">
-                <h4 id="startCardTitle" class="card-title mb-3">
-                  Web Applications
-                </h4>
-                <p class="card-text1">
-                  As a web developer, I create full scale web applications that
-                  can help your product stand out.
-                </p>
-                <a
-                  href="https://celestial-chronicles.netlify.app/"
-                  target="_blank"
-                  class="btn btn-primary btn1">
-                  Read More
-                </a>
+    <section id="portfolio" className="p-5">
+      <div className="container projects">
+        <h2 className="text-center p-4"> Recent Projects</h2>
+        <div className="row text-center g-4">
+          {Pikachu.map((props) => {
+            return (
+              <div className="col-md">
+                <div className="card bg-dark text-light projectCard1">
+                  <img
+                    src={props.image}
+                    className="w-100 card1-img"
+                    alt="This is an image of my first project, which was a refactoring work for a marketing company called Horiseon - The image is showing their landing page"
+                  />
+                  <div className="card-body text-center">
+                    <h4 id="startCardTitle" className="card-title mb-3">
+                      {props.title}
+                    </h4>
+                    <p className="card-text1">{props.description}</p>
+                    <a
+                      href={props.link}
+                      target="_blank"
+                      className="btn btn-primary btn1">
+                      Read More
+                    </a>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div class="col-md">
-            <div class="card bg-dark text-light projectCard2">
-              <img
-                src={ResponsiveDesign}
-                class="w-100 card2-img"
-                alt="This is an image of my third project, which is my personal portfolio website you are currently viewing - It was done using HTML, CSS and Bootstrap"
-              />
-              <div class="card-body text-center">
-                <h4 id="middleCardTitle" class="card-title mb-3">
-                  Functionality & Responsive Design
-                </h4>
-                <p class="card-text2">
-                  I harness Bootstrap, JavaScript, and other frameworks in web
-                  development, ensuring enhanced functionality and responsive
-                  design.
-                </p>
-                <a
-                  href="https://tjl-bootstrap.netlify.app/"
-                  target="_blank"
-                  class="btn btn-primary">
-                  Read More
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md">
-            <div class="card bg-dark text-light projectCard3">
-              <img
-                src={PortfolioWebsite}
-                class="w-100 card3-img"
-                alt="This is an image of my second project, which is my personal portfolio - It was done using only HTML and CSS"
-              />
-              <div class="card-body text-center">
-                <h4 id="endCardTitle" class="card-title mb-3">
-                  Portfolio Websites
-                </h4>
-                <p class="card-text3">
-                  Designing personal portfolio websites to elegantly display
-                  skills, experiences & personality, leaving a remarkable
-                  digital footprint in seconds.
-                </p>
-                <a
-                  href="https://cerulean-gaufre-fd953a.netlify.app/"
-                  target="_blank"
-                  class="btn btn-primary">
-                  Read More
-                </a>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </section>

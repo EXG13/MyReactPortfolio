0b00796fe6a115ace8fff81fd555cb4754e2124f
Tomer J. Lind <digitalassetshub@gmail.com>
2024-01-19 19:39:53 +0200

    changes to routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import About from "./components/About"
-import Portfolio from "./components/Projects";
-
-
-
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Home />
-      <About/>
-      <Portfolio/>
+      <About />
+      <Projects />
+      <Contact/>
     </Router>
   );
 }
 
-export default App
+export default App;

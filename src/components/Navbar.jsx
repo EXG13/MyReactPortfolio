1be2e03c246c83dbf9c7966e85e1368cbb5572d9
Tomer J. Lind <digitalassetshub@gmail.com>
2024-01-19 14:34:30 +0200

    added Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-dark navbar-dark py-3">
+      <div className="container">
+        <a href="#" className="navbar-brand">
+          BrandLabs3.0
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navmenu">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navmenu">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a href="#about" className="nav-link">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#portfolio" className="nav-link">
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#contact" className="nav-link">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+
+export default Navbar
